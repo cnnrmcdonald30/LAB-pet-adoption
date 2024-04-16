68dc69ef97b712d24d726a4a613cb9ded42a9f49
cnnrmcdonald30 <cnnrmcdonald30@gmail.com>
2024-04-16 16:58:17 -0500

    finished html

--- main.js
+++ main.js
@@ -240,3 +240,22 @@ const pets = [
       imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
     }
   ];
+
+  const targetingApp = document.querySelector('#pet')
+
+  let domString = ""
+  pets.forEach(pet => {
+    domString+= `
+  <div class="card" style="width: 18rem; display: flex; margin: 10px">
+    <img src="${pet.imageUrl}" class="card-img-top" alt="${pet.name}">
+    <div class="card-body">
+      <h5 class="card-title">${pet.name}</h5>
+      <p class="card-text">${pet.specialSkill}</p>
+      <a href="#" class="btn btn-success">Cat</a>
+
+    </div>
+  </div>`;
+  });
+targetingApp.innerHTML = domString;
+console.log('testing');
+
